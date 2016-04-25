e7cf9126cdd31a4174f63c0318874cac662c9d6b
luca28pet <lucapetrucci28@yahoo.it>
2016-04-25 21:30:56 +0200

    Optimizations

--- src/AntiCheatPE/EventListener.php
+++ src/AntiCheatPE/EventListener.php
@@ -18,81 +18,83 @@ class EventListener implements Listener{
 
     public function onMove(PlayerMoveEvent $event){
         $p = $event->getPlayer();
+        $name = $event->getPlayer()->getName();
 
         if($p->isCreative() or $p->isSpectator() or $p->getAllowFlight() or $p->hasEffect(8) or $p->hasPermission("anticheat.admin")) return;
 
-        if(!Main::isAirUnder($p) and isset($this->antiCheat->isElevating[$p->getId()])){
-            unset($this->antiCheat->isElevating[$p->getId()]);
-        }
+        $isAirUnder = Main::isAirUnder($p);
 
-        $fromY = $event->getFrom()->y;
-        $toY = $event->getTo()->y;
-        $name = $event->getPlayer()->getName();
+        if(!$isAirUnder and isset($this->antiCheat->isElevating[$name])){
+            unset($this->antiCheat->isElevating[$name]);
+        }else{
+            $fromY = $event->getFrom()->y;
+            $toY = $event->getTo()->y;
 
-        if($toY < $fromY and isset($this->antiCheat->isElevating[$p->getId()])){
-            $this->antiCheat->isElevating[$p->getId()] -= $fromY - $toY;
-            if($this->antiCheat->isElevating[$p->getId()] <= 0){
-                unset($this->antiCheat->isElevating[$p->getId()]);
+            if($toY < $fromY and isset($this->antiCheat->isElevating[$name])){
+                $this->antiCheat->isElevating[$name] -= $fromY - $toY;
+                if($this->antiCheat->isElevating[$name] <= 0){
+                    unset($this->antiCheat->isElevating[$name]);
+                }
             }
-        }
 
-        elseif($toY > $fromY){
-            isset($this->antiCheat->isElevating[$p->getId()]) ?
-                $this->antiCheat->isElevating[$p->getId()] += $toY - $fromY
-                :
-                $this->antiCheat->isElevating[$p->getId()] = $toY - $fromY
-            ;
+            elseif($toY > $fromY){
+                isset($this->antiCheat->isElevating[$name]) ?
+                    $this->antiCheat->isElevating[$name] += $toY - $fromY
+                    :
+                    $this->antiCheat->isElevating[$name] = $toY - $fromY
+                ;
 
-            if(
-                Main::isAirUnder($p) and
-                $this->antiCheat->isElevating[$p->getId()] > 1.5
+                if(
+                    $isAirUnder and
+                    $this->antiCheat->isElevating[$name] > 1.5
+                ){
+                    $this->antiCheat->players[$name]++;
+                }else{
+                    $this->antiCheat->players[$name] = 0;
+                }
+            }
+
+            elseif(
+                round($fromY, 5) === round($toY, 5) and
+                $isAirUnder
             ){
                 $this->antiCheat->players[$name]++;
             }else{
                 $this->antiCheat->players[$name] = 0;
             }
-        }
 
-        elseif(
-            round($fromY, 5) === round($toY, 5) and
-            Main::isAirUnder($p)
-        ){
-            $this->antiCheat->players[$name]++;
-        }else{
-            $this->antiCheat->players[$name] = 0;
-        }
-
-        if(isset($this->antiCheat->players[$name]) and $this->antiCheat->players[$name] === $this->antiCheat->options["tags"]){
-            unset($this->antiCheat->players[$name]);
-            $this->antiCheat->kicks[$name]++;
-            $event->getPlayer()->kick($this->antiCheat->options["kick message"], false);
-            return;
-        }
-
-        if(isset($this->antiCheat->kicks[$name]) and $this->antiCheat->kicks[$name] === $this->antiCheat->options["kicks"]){
-            unset($this->antiCheat->kicks[$name]);
-            switch($this->antiCheat->options["Action"]){
-                case "ban-ip":
-                    $this->antiCheat->getServer()->getIPBans()->addBan($event->getPlayer()->getAddress());
-                    $event->getPlayer()->kick(TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false);
-                    break;
-                case "ban":
-                    $this->antiCheat->getServer()->getNameBans()->addBan($event->getPlayer()->getName());
-                    $event->getPlayer()->kick(TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false);
-                    break;
-                case "ban-client":
-                    if(($banclientplugin = $this->antiCheat->getServer()->getPluginManager()->getPlugin("BanClient")) !== null){
-                        $banclientplugin->banClient($event->getPlayer(), TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false, true);
-                    }else{
-                        $this->antiCheat->getServer()->getLogger()->warning("[AntiCheat] BanClient plugin not found!");
-                    }
-                    break;
-                case "custom":
-                    foreach($this->antiCheat->options["Commands"] as $commands){
-                        $this->antiCheat->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $event->getPlayer()->getName(), $commands));
-                    }
+            if(isset($this->antiCheat->players[$name]) and $this->antiCheat->players[$name] === $this->antiCheat->options["tags"]){
+                unset($this->antiCheat->players[$name]);
+                $this->antiCheat->kicks[$name]++;
+                $event->getPlayer()->kick($this->antiCheat->options["kick message"], false);
+                return;
             }
-            return;
+
+            if(isset($this->antiCheat->kicks[$name]) and $this->antiCheat->kicks[$name] === $this->antiCheat->options["kicks"]){
+                unset($this->antiCheat->kicks[$name]);
+                switch($this->antiCheat->options["Action"]){
+                    case "ban-ip":
+                        $this->antiCheat->getServer()->getIPBans()->addBan($event->getPlayer()->getAddress());
+                        $event->getPlayer()->kick(TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false);
+                        break;
+                    case "ban":
+                        $this->antiCheat->getServer()->getNameBans()->addBan($event->getPlayer()->getName());
+                        $event->getPlayer()->kick(TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false);
+                        break;
+                    case "ban-client":
+                        if(($banclientplugin = $this->antiCheat->getServer()->getPluginManager()->getPlugin("BanClient")) !== null){
+                            $banclientplugin->banClient($event->getPlayer(), TextFormat::RED . "[AntiCheat] " . TextFormat::YELLOW . "You were banned for using mods/hacks.", false, true);
+                        }else{
+                            $this->antiCheat->getServer()->getLogger()->warning("[AntiCheat] BanClient plugin not found!");
+                        }
+                        break;
+                    case "custom":
+                        foreach($this->antiCheat->options["Commands"] as $commands){
+                            $this->antiCheat->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $event->getPlayer()->getName(), $commands));
+                        }
+                }
+                return;
+            }
         }
 
         if($p->hasEffect(Effect::SPEED) or $p->hasPermission("anticheat.admin")) return;
